4e574956c1303af8abfe1c4b4f1c09ee973d076e
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-04-24 16:45:45 +0500

    structure

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <div >
+      <h1>My Portfolio</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
