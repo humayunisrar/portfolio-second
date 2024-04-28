1d0d52a26209f770e245841945344e0e8418fd7b
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-04-28 23:57:13 +0500

    .

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -9,7 +9,6 @@ const Navbar = () => {
 
   const [click, setClick] = React.useState(false);
   const handleClick = () => setClick(!click);
-  const [color,setColor]=useState(false);
   const changeColor=()=>{
     if(window.scrollY>=80){
       setColor(true);
