b1ed594ed0edd9e29d69838643193911d943531d
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-05-01 19:37:38 +0500

    hero second

---
+++ portfolio/src/components/Hero2.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Hero2.css"
+
+const Hero2 = () => {
+  return (
+    <div className='hero-img'>
+        <div className='heading'>
+            <h1>heading</h1>
+            <p>paragraph</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero2
