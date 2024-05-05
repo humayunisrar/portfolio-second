ab933420f68161479d2f3de3259bbd574d6230d3
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-05-05 19:51:11 +0500

    personalization

--- portfolio/src/components/WorkData.js
+++ portfolio/src/components/WorkData.js
@@ -1,28 +1,46 @@
 import sleet from "../assets/sleet.jpg";
 import nextgen from "../assets/nextgen.jpg";
 import senior from "../assets/senior.jpg";
+import slb2 from "../assets/SLB2.png";
+import ameera2 from "../assets/Ameera2.png";
+
+
 
 const ProjectData = [
     {
         imgsrc: sleet,
         title: "Sleet",
-        text: "A weather app that provides real-time weather data for any location in the world.",
-        view: "https://sleet.netlify.app/",
+        text: "Sleet is the pioneering cold store logistics solution to be Saudi Arabia based that changes the way that logistics operates by relentless innovation and digitalized approaches.",
+        view: "https://sleet-sa.com/",
         
     },
 
     {
         imgsrc: nextgen,
         title: "NextGen-Women",
-        text: "A weather app that provides real-time weather data for any location in the world.",
-        view: "https://sleet.netlify.app/",
+        text: "NextGen Women has built a platform for successful women to transfer their knowledge and skills onto the next generation!",
+        view: "https://nextgen-women.com/",
+        
+    },
+    {
+        imgsrc: slb2,
+        title: "SLB Group Inc",
+        text: "At SLB Group, they focus on delivering solutions grounded in data, efficiency, and process improvement. Their tailored approach not only targets hard cost savings but also addresses the often-overlooked soft cost savings crucial for small to medium-sized business owners.",
+        view: "https://slbgroupinc.com/",
+        
+    },
+    {
+        imgsrc: ameera2,
+        title: "Ameera Art",
+        text: "Ameera Art, a platform dedicated to showcasing expertise in English calligraphy. Residing in KSA, and an expert in the art of English calligraphy.",
+        view: "https://www.ameeraart.com/",
         
     },
     {
         imgsrc: senior,
         title: "Senior-Investigators",
-        text: "A weather app that provides real-time weather data for any location in the world.",
-        view: "https://sleet.netlify.app/",
+        text: "enior Investigations is a reputable Gold Coast-based private investigation agency specialising in a wide range of investigation services including background checks, factual investigations, surveillance, genealogy, and skip tracing.",
+        view: "https://www.seniorinvestigations.com.au/",
         
     }
 ]
